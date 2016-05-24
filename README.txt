dcc0184dcc2d781447cf1faf9ab2b7aea3d9f564
Joshua Mora <jshm101@gmail.com>
2016-05-24 14:26:47 -0700

    Script is done... have used the script to scrape every team's shooting data for every season 2010->2015. We have our data

--- README.txt
+++ README.txt
@@ -32,3 +32,18 @@ to pull data from the server. This URL takes different arguments
 specifying the team, season, etc. The response from this URL
 contains all the data we need (team, player, shot location
 in x-, y-coordinates) in JSON format.
+
+
+How to use script:
+
+1) You must grab the NBASavant API URL for the specific team you are
+trying to scrape data from. This URL will go in the "url" variable
+
+2) You must supply the name of the team you are scraping data from
+in the variable "teamWeAreSraping". This is done for file writing
+purposes so that the stats are written to the correct team folders.
+
+3) Run the script as described above, navigate to localhost:8081/scrape
+and watch the command line. After the command line has finished writing
+2010 stats, you can go ahead and kill the server (CTRL-C). The data
+will be written at that point inside of the folder "team_data"
--- server/server.js
+++ server/server.js
@@ -9,19 +9,58 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 var app     = express();
+var yearToWrite = 2015;
+var teamWeAreScraping;
+var server;
 
 
 // navigate to localhost:8188/scrape
 // to begin scraping from NBASavant API.
 app.get('/scrape', function(req, res){
-   //All the web scraping magic will happen here
+   
+   url = "http://nbasavant.com/ajax/getShotsCompare.php?ids=1610612764%7C&st=&q1=&q2=&sza=&szb=&sd1=&ed1=&sd2=&ed2=&szr=&y1=2014&y2=2014&dgt=&dlt=&defdistgt1=&defdistlt1=&defdistgt2=&defdistlt2=&team=&min_gt=&min_lt=&sec_gt=&sec_lt=&shot_made=&shot_made_p2=&sp=false&gb1=player&gb2=player&_=1464124818705"
    
    
-   // test URL for now that retrieves Lakers shot chart info
-   // for 2010->2011 season
-   url = "http://nbasavant.com/ajax/getShotsCompare.php?ids=1610612741%7C&st=&q1=&q2=&sza=&szb=&sd1=&ed1=&sd2=&ed2=&szr=&y1=2014&y2=2014&dgt=&dlt=&defdistgt1=&defdistlt1=&defdistgt2=&defdistlt2=&team=&min_gt=&min_lt=&sec_gt=&sec_lt=&shot_made=&shot_made_p2=&sp=false&gb1=player&gb2=player&_=1464070777032"
- 
+   // grab the API URL for the team we are getting data
+   // for and set that team's name for this variable
+   // this variable will be used in finding location to save data.
+   teamWeAreScraping = "wizards";
+   
+   
+   // break the URL up to automate grabbing data for all
+   // years (2010 -> 2015) for some particular team.
+   var urlBeforeYear = url.substring(0, 110);
+   console.log("urlBeforeYear: " + urlBeforeYear);   
+   console.log(urlBeforeYear.length);   
+   var theYear;      
+   var urlAfterYear = url.substring(118, url.length);
+   console.log("urlAfterYear: " +urlAfterYear);
+   
+   var urlList = [];
+   var results = [];
+   
+   
+   // construct all of the URLs for data scraping
+   // where each URL specifies a year/season of data.
+   for(var i = 2010; i < 2016; i++){
+      theYear = "&y1=" + i;
+      console.log("theYear: " + theYear);
+      
+      url = urlBeforeYear + theYear + urlAfterYear;
+      urlList.push(url);
+   }
+   if(urlList){
+      syncCalls(urlList);
+   }
+   
+})
+
+// we want to synchronize the URL calls so that we can
+// keep track of proper files to save data to.
+function syncCalls(urls){
+   url = urls.pop();
    // perform the request.
    request(url, function(error, response, html){
      if(!error){
@@ -40,6 +79,8 @@ app.get('/scrape', function(req, res){
         var $ = cheerio.load(html);
                      
         $('script').filter(function(){
+           var j = yearToWrite;
+           
            
            var data = $(this);
            console.log("type of data is: " +typeof(data));
@@ -68,7 +109,24 @@ app.get('/scrape', function(req, res){
            jsonStats = jsonStats + "]";
            
            //console.log(jsonStats);
-           //console.log(jsonStats.substring(0, 15));
+           console.log(jsonStats.substring(0, 75));
+           
+           var fileToWrite = "team_data/" + teamWeAreScraping + "/" + j;
+           console.log("fileToWrite: " + fileToWrite);
+           fs.writeFile(fileToWrite, jsonStats, function(err){
+              if (err){
+                 return console.log(err);  
+              }
+              console.log("The file was saved!");
+              yearToWrite--;
+              
+              // still have URLs to make requests to.
+              if(urls.length > 0){
+                 syncCalls(urls);   
+              }else{
+                  server.close();  
+              }
+           });
            
            //console.log("jsonStats: " + jsonStats);
         });
@@ -81,10 +139,10 @@ app.get('/scrape', function(req, res){
         console.log("an error has occurred: " +error);  
             
      }
-     });
-})
+   });    
+}
 
-app.listen('8081')
+server = app.listen('8081')
 
 console.log('Magic happens on port 8081');
 
